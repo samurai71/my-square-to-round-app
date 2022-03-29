0be6780106816afd8ce5583b3884afcdba552862
Mark Landeryou <samurai96@hotmail.com>
2022-03-29 16:07:18 -0400

    created a test for the Form component in Jest

---
+++ src/components/Form.test.js
@@ -0,0 +1,7 @@
+//import { render, screen } from '@testing-library/react';
+//import App from '../App';
+//const { mul } = require('./Form');
+
+test('2 * 2 * 2 = 8', () => {
+    expect(2 * 2 * 2).toBe(8);
+});
